e65574073bb954057dc9364cbf4611bacc61a3f3
Kiarash Korki <kiarash@korki.me>
2018-01-23 19:04:55 +0330

    Add Unit class

---
+++ client/src/Model/Unit.cpp
@@ -0,0 +1,87 @@
+#include "Unit.h"
+
+Unit::Unit(UnitType type, int health, int level, int price, int added_income, int bounty, int damage, int move_speed,
+           int vision_range)
+        : m_type(type)
+        , m_health(health)
+        , m_level(level)
+        , m_price(price)
+        , m_added_income(added_income)
+        , m_bounty(bounty)
+        , m_damage(damage)
+        , m_move_speed(move_speed)
+        , m_vision_range(vision_range)
+{
+}
+
+void Unit::set_type(UnitType type) {
+    m_type = type;
+}
+
+void Unit::set_health(int health) {
+    m_health = health;
+}
+
+void Unit::set_level(int level) {
+    m_level = level;
+}
+
+void Unit::set_price(int price) {
+    m_price = price;
+}
+
+void Unit::set_added_income(int added_income) {
+    m_added_income = added_income;
+}
+
+void Unit::set_bounty(int bounty) {
+    m_bounty = bounty;
+}
+
+void Unit::set_damage(int damage) {
+    m_damage = damage;
+}
+
+void Unit::set_move_speed(int move_speed) {
+    m_move_speed = move_speed;
+}
+
+void Unit::set_vision_range(int vision_range) {
+    m_vision_range = vision_range;
+}
+
+UnitType Unit::get_type() const {
+    return m_type;
+}
+
+int Unit::get_health() const {
+    return m_health;
+}
+
+int Unit::get_level() const {
+    return m_level;
+}
+
+int Unit::get_price() const {
+    return m_price;
+}
+
+int Unit::get_added_income() const {
+    return m_added_income;
+}
+
+int Unit::get_bounty() const {
+    return m_bounty;
+}
+
+int Unit::get_damage() const {
+    return m_damage;
+}
+
+int Unit::get_move_speed() const {
+    return m_move_speed;
+}
+
+int Unit::get_vision_range() const {
+    return m_vision_range;
+}
---
+++ client/src/Model/Unit.h
@@ -0,0 +1,79 @@
+#ifndef AIC18_CLIENT_CPP_UNIT_H
+#define AIC18_CLIENT_CPP_UNIT_H
+
+#include "Entity.h"
+
+/**
+ * Type of a unit
+ */
+enum class UnitType {
+    HEAVY,
+    LIGHT
+};
+
+/**
+ * An attacking unit in the game
+ */
+class Unit : public Entity {
+public:
+
+    Unit() = default;
+    ~Unit() override = default;
+
+    Unit(const Unit&) = default;
+    Unit& operator= (const Unit&) = default;
+
+    Unit(UnitType type, int health, int level, int price, int added_income, int bounty, int damage, int move_speed,
+        int vision_range);
+
+    void set_type(UnitType type);
+    void set_health(int health);
+    void set_level(int level);
+    void set_price(int price);
+    void set_added_income(int added_income);
+    void set_bounty(int bounty);
+    void set_damage(int damage);
+    void set_move_speed(int move_speed);
+    void set_vision_range(int vision_range);
+
+    UnitType get_type() const;
+    int get_health() const;
+    int get_level() const;
+    int get_price() const;
+    int get_added_income() const;
+    int get_bounty() const;
+    int get_damage() const;
+    int get_move_speed() const;
+    int get_vision_range() const;
+
+private:
+
+    /// The type of this unit
+    UnitType m_type;
+
+    /// Current amount of health
+    int m_health;
+
+    /// Current level
+    int m_level;
+
+    /// Price to build this unit
+    int m_price;
+
+    /// Amount of income added to player's income after creating this unit
+    int m_added_income;
+
+    /// The money rewarded to the enemy if this unit is killed
+    int m_bounty;
+
+    /// Amount of damage this unit does to enemy's health if it passes the path
+    int m_damage;
+
+    /// Number of time cycles this unit needs to pass a single cell on map
+    int m_move_speed;
+
+    /// Maximum distance of map cells this unit sees
+    int m_vision_range;
+};
+
+#endif // AIC18_CLIENT_CPP_UNIT_H
