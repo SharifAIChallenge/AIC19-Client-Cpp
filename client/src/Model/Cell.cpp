6b9e957ca073940bff84895628d094c41bc2da2e
Kiarash Korki <kiarash@korki.me>
2018-01-24 20:37:53 +0330

    Add Cell class
        By using shared_ptrs we share the ownership with users. This way
        they don't need to worry about null-pointers/invalid-references,
        even if they copy the objects for themselves.

---
+++ client/src/Model/Cell.cpp
@@ -0,0 +1,48 @@
+#include "Cell.h"
+
+#include <algorithm>
+#include <functional>
+
+Cell::Cell(CellType type, Point location)
+        : m_type(type)
+        , m_location(location)
+{
+}
+
+void Cell::set_type(CellType type) {
+    m_type = type;
+}
+
+void Cell::set_location(Point location) {
+    m_location = location;
+}
+
+CellType Cell::get_type() const {
+    return m_type;
+}
+
+Point Cell::get_location() const {
+    return m_location;
+}
+
+Cell::UnitList& Cell::get_units() {
+    return m_units;
+}
+
+Cell::ConstUnitList Cell::get_units() const {
+    ConstUnitList result(m_units.size());
+    std::transform(m_units.begin(), m_units.end(), result.begin(), [] (auto x) { return x; });
+    return result;
+}
+
+void Cell::set_tower(const std::shared_ptr<Tower>& tower) {
+    m_tower = tower;
+}
+
+bool Cell::has_tower() const {
+    return m_tower != nullptr;
+}
+
+std::shared_ptr<const Tower> Cell::get_tower() const {
+    return m_tower;
+}
---
+++ client/src/Model/Cell.h
@@ -0,0 +1,66 @@
+#ifndef AIC18_CLIENT_CPP_CELL_H
+#define AIC18_CLIENT_CPP_CELL_H
+
+#include <vector>
+#include <memory>
+
+#include "Point.h"
+#include "Unit.h"
+#include "Tower.h"
+
+/**
+ * Type of a map cell
+ */
+enum class CellType {
+    ROAD,
+    GRASS,
+    BLOCK
+};
+
+/**
+ * A single cell in game map grid
+ */
+class Cell {
+
+    using UnitList = std::vector<std::shared_ptr<Unit>>;
+    using ConstUnitList = std::vector<std::shared_ptr<const Unit>>;
+
+public:
+
+    Cell() = default;
+    ~Cell() = default;
+
+    Cell(const Cell&) = default;
+    Cell& operator= (const Cell&) = default;
+
+    Cell(CellType type, Point location);
+
+    void set_type(CellType type);
+    void set_location(Point location);
+
+    CellType get_type() const;
+    Point get_location() const;
+
+    UnitList& get_units();
+    ConstUnitList get_units() const;
+
+    void set_tower(const std::shared_ptr<Tower>& tower);
+    bool has_tower() const;
+    std::shared_ptr<const Tower> get_tower() const;
+
+private:
+
+    /// The type of this cell
+    CellType m_type;
+
+    /// Location of this cell on game map
+    Point m_location;
+
+    /// List of units currently located on this cell
+    std::vector<std::shared_ptr<Unit>> m_units;
+
+    /// An optional tower that may be built on this cell
+    std::shared_ptr<Tower> m_tower;
+};
+
+#endif // AIC18_CLIENT_CPP_CELL_H
