c3573a29bedbd632113603f5f035704069fa811a
Kiarash Korki <kiarash@korki.me>
2018-02-02 17:45:37 +0330

    Upgrade Map interface

--- client/src/Model/Map.cpp
+++ client/src/Model/Map.cpp
@@ -1,97 +1,43 @@
 #include "Map.h"
 
-Map::Map(size_t width, size_t height, const std::vector<Path>& paths) {
-    // TODO: Implement this
+Map::Map(const SharedPtrGrid<Cell>& cells_grid)
+        : m_cells_grid(cells_grid)
+{
 }
 
-Map::Map(const std::vector<std::vector<Cell>>& cells, const std::vector<Path>& paths) {
-    // TODO: Implement this
+void Map::set_cells_grid(const SharedPtrGrid<Cell>& cells_grid) {
+    m_cells_grid = cells_grid;
 }
 
-void Map::set_width(size_t width) {
-    // TODO: Implement this
+const SharedPtrGrid<Cell>& Map::get_cells_grid() {
+    return m_cells_grid;
 }
 
-void Map::set_height(size_t height) {
-    // TODO: Implement this
+SharedPtrGrid<const Cell> Map::get_cells_grid() const {
+    SharedPtrGrid<const Cell> result;
+    for (const auto& row : m_cells_grid)
+        result.emplace_back(row.begin(), row.end());
+    return result;
 }
 
-void Map::set_cells(const std::vector<std::vector<Cell>>& cells) {
-    // TODO: Implement this
+SharedPtrList<Cell> Map::get_cells_list() {
+    SharedPtrList<Cell> result;
+    for (const auto& row : m_cells_grid)
+        result.insert(result.end(), row.begin(), row.end());
+    return result;
 }
 
-void Map::set_my_units(const std::vector<Unit>& my_units) {
-    // TODO: Implement this
+SharedPtrList<const Cell> Map::get_cells_list() const {
+    SharedPtrList<const Cell> result;
+    for (const auto& row : m_cells_grid)
+        result.insert(result.end(), row.begin(), row.end());
+    return result;
 }
 
-void Map::set_enemy_units(const std::vector<Unit>& enemy_units) {
-    // TODO: Implement this
+std::shared_ptr<Cell> Map::get_cell(Point location) {
+    return m_cells_grid[location.get_x()][location.get_y()];
 }
 
-void Map::set_my_towers(const std::vector<Tower>& my_towers) {
-    // TODO: Implement this
-}
-
-void Map::set_visible_enemy_towers(const std::vector<Tower>& visible_enemy_towers) {
-    // TODO: Implement this
-}
-
-void Map::set_paths(const std::vector<Path>& paths) {
-    // TODO: Implement this
-}
-
-size_t Map::get_width() const {
-    // TODO: Implement this
-}
-
-size_t Map::get_height() const {
-    // TODO: Implement this
-}
-
-std::vector<std::vector<Cell>>& Map::get_cells() {
-    // TODO: Implement this
-}
-
-const std::vector<std::vector<Cell>>& Map::get_cells() const {
-    // TODO: Implement this
-}
-
-std::vector<std::shared_ptr<Unit>>& Map::get_my_units() {
-    // TODO: Implement this
-}
-
-std::vector<std::shared_ptr<const Unit>> Map::get_my_units() const {
-    // TODO: Implement this
-}
-
-std::vector<std::shared_ptr<Unit>>& Map::get_enemy_units() {
-    // TODO: Implement this
-}
-
-std::vector<std::shared_ptr<const Unit>> Map::get_enemy_units() const {
-    // TODO: Implement this
-}
-
-std::vector<std::shared_ptr<Tower>>& Map::get_my_towers() {
-    // TODO: Implement this
-}
-
-std::vector<std::shared_ptr<const Tower>> Map::get_my_towers() const {
-    // TODO: Implement this
-}
-
-std::vector<std::shared_ptr<Tower>>& Map::get_visible_enemy_towers() {
-    // TODO: Implement this
-}
-
-std::vector<std::shared_ptr<const Tower>> Map::get_visible_enemy_towers() const {
-    // TODO: Implement this
-}
-
-std::vector<Path>& Map::get_paths() {
-    // TODO: Implement this
-}
-
-const std::vector<Path>& Map::get_paths() const {
-    // TODO: Implement this
+std::shared_ptr<const Cell> Map::get_cell(Point location) const {
+    return std::const_pointer_cast<const Cell>(m_cells_grid[location.get_x()][location.get_y()]);
 }
--- client/src/Model/Map.h
+++ client/src/Model/Map.h
@@ -1,13 +1,9 @@
 #ifndef AIC18_CLIENT_CPP_MAP_H
 #define AIC18_CLIENT_CPP_MAP_H
 
-#include <vector>
-#include <memory>
-
+#include "../Core/Utility.h"
 #include "Cell.h"
-#include "Unit.h"
-#include "Tower.h"
-#include "Path.h"
+#include "Point.h"
 
 /**
  * A 2d grid of @see Cell objects
@@ -19,63 +15,26 @@ public:
     ~Map() = default;
 
     Map(const Map&) = default;
-    Map& operator= (const Map&) = default;
+    Map& operator=(const Map&) = default;
 
-    Map(size_t width, size_t height, const std::vector<Path>& paths);
-    Map(const std::vector<std::vector<Cell>>& cells, const std::vector<Path>& paths);
+    explicit Map(const SharedPtrGrid<Cell>& cells_grid);
 
-    void set_width(size_t width);
-    void set_height(size_t height);
-    void set_cells(const std::vector<std::vector<Cell>>& cells);
-    void set_my_units(const std::vector<Unit>& my_units);
-    void set_enemy_units(const std::vector<Unit>& enemy_units);
-    void set_my_towers(const std::vector<Tower>& my_towers);
-    void set_visible_enemy_towers(const std::vector<Tower>& visible_enemy_towers);
-    void set_paths(const std::vector<Path>& paths);
+    void set_cells_grid(const SharedPtrGrid<Cell>& cells_grid);
 
-    size_t get_width() const;
-    size_t get_height() const;
+    const SharedPtrGrid<Cell>& get_cells_grid();
+    SharedPtrGrid<const Cell> get_cells_grid() const;
 
-    std::vector<std::vector<Cell>>& get_cells();
-    const std::vector<std::vector<Cell>>& get_cells() const;
+    SharedPtrList<Cell> get_cells_list();
+    SharedPtrList<const Cell> get_cells_list() const;
 
-    std::vector<std::shared_ptr<Unit>>& get_my_units();
-    std::vector<std::shared_ptr<const Unit>> get_my_units() const;
-
-    std::vector<std::shared_ptr<Unit>>& get_enemy_units();
-    std::vector<std::shared_ptr<const Unit>> get_enemy_units() const;
-
-    std::vector<std::shared_ptr<Tower>>& get_my_towers();
-    std::vector<std::shared_ptr<const Tower>> get_my_towers() const;
-
-    std::vector<std::shared_ptr<Tower>>& get_visible_enemy_towers();
-    std::vector<std::shared_ptr<const Tower>> get_visible_enemy_towers() const;
-
-    std::vector<Path>& get_paths();
-    const std::vector<Path>& get_paths() const;
+    std::shared_ptr<Cell> get_cell(Point location);
+    std::shared_ptr<const Cell> get_cell(Point location) const;
 
 private:
 
-    /// Map dimensions
-    size_t m_width , m_height;
+    /// 2d array of map cells
+    SharedPtrGrid<Cell> m_cells_grid;
 
-    /// The actual cells of the map
-    std::vector<std::vector<Cell>> m_cells;
-
-    /// List of my units
-    std::vector<std::shared_ptr<Unit>> m_my_units;
-
-    /// List of enemy units
-    std::vector<std::shared_ptr<Unit>> m_enemy_units;
-
-    /// List of my towers
-    std::vector<std::shared_ptr<Tower>> m_my_towers;
-
-    /// List of enemy towers that are visible in this cycle
-    std::vector<std::shared_ptr<Tower>> m_visible_enemy_towers;
-
-    /// List of paths on the game map
-    std::vector<Path> m_paths;
 };
 
 #endif // AIC18_CLIENT_CPP_MAP_H
