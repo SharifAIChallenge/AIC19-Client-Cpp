2fd12a79d1703ec52a814201bc87966545ebf81e
Kiarash Korki <kiarash@korki.me>
2018-02-02 22:29:45 +0330

    Add AI class

---
+++ client/src/AI/AI.cpp
@@ -0,0 +1,9 @@
+#include "AI.h"
+
+void AI::simple_turn(World& world) {
+    // TODO: Implement this
+}
+
+void AI::complex_turn(World& world) {
+    // TODO: Implement this
+}
---
+++ client/src/AI/AI.h
@@ -0,0 +1,15 @@
+#ifndef AIC18_CLIENT_CPP_AI_H
+#define AIC18_CLIENT_CPP_AI_H
+
+#include "../Model/World.h"
+
+class AI {
+public:
+
+    void simple_turn(World& world);
+
+    void complex_turn(World& world);
+
+};
+
+#endif // AIC18_CLIENT_CPP_AI_H
