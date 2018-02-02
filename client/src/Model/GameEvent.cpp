0818a74e17f36b9d83a10da7d759e14e3744e3ea
Kiarash Korki <kiarash@korki.me>
2018-02-02 17:57:22 +0330

    Upgrade GameEvent interface

--- client/src/Model/GameEvent.cpp
+++ client/src/Model/GameEvent.cpp
@@ -1,14 +1,9 @@
 #include "GameEvent.h"
 
-GameEvent::GameEvent(EventType type, Owner owner, Point location)
-        : m_type(type)
-        , m_owner(owner)
+GameEvent::GameEvent(Owner owner, Point location)
+        : m_owner(owner)
         , m_location(location)
 {
-}
-
-void GameEvent::set_type(EventType type) {
-    m_type = type;
 }
 
 void GameEvent::set_owner(Owner owner) {
@@ -19,14 +14,20 @@ void GameEvent::set_location(Point location) {
     m_location = location;
 }
 
-EventType GameEvent::get_type() const {
-    return m_type;
-}
-
 Owner GameEvent::get_owner() const {
     return m_owner;
 }
 
 Point GameEvent::get_location() const {
     return m_location;
+}
+
+StormEvent::StormEvent(Owner owner, Point location)
+        : GameEvent(owner, location)
+{
+}
+
+BeanEvent::BeanEvent(Owner owner, Point location)
+        : GameEvent(owner, location)
+{
 }
--- client/src/Model/GameEvent.h
+++ client/src/Model/GameEvent.h
@@ -1,49 +1,60 @@
 #ifndef AIC18_CLIENT_CPP_GAME_EVENT_H
 #define AIC18_CLIENT_CPP_GAME_EVENT_H
 
+#include "Player.h"
 #include "Point.h"
-#include "Entity.h"
 
 /**
- * Type of an event
- */
-enum class EventType {
-    BEAN_EVENT,
-    STORM_EVENT
-};
-
-/**
- * An event happened in one cycle
+ * An event happened in one turn
  */
 class GameEvent {
 public:
 
     GameEvent() = default;
-    ~GameEvent() = default;
+    virtual ~GameEvent() = default;
 
     GameEvent(const GameEvent&) = default;
     GameEvent& operator= (const GameEvent&) = default;
 
-    GameEvent(EventType type, Owner owner, Point location);
+    GameEvent(Owner owner, Point location);
 
-    void set_type(EventType type);
     void set_owner(Owner owner);
     void set_location(Point location);
 
-    EventType get_type() const;
     Owner get_owner() const;
     Point get_location() const;
 
 private:
-
-    /// Bean or storm?
-    EventType m_type;
 
     /// Who caused the event
     Owner m_owner;
 
     /// Location for this event
     Point m_location;
+};
+
+class StormEvent : public GameEvent {
+public:
+
+    StormEvent() = default;
+    ~StormEvent() override = default;
+
+    StormEvent(const StormEvent&) = default;
+    StormEvent& operator= (const StormEvent&) = default;
+
+    StormEvent(Owner owner, Point location);
+};
+
+class BeanEvent : public GameEvent {
+public:
+
+    BeanEvent() = default;
+    ~BeanEvent() override = default;
+
+    BeanEvent(const BeanEvent&) = default;
+    BeanEvent& operator= (const BeanEvent&) = default;
+
+    BeanEvent(Owner owner, Point location);
 };
 
 #endif // AIC18_CLIENT_CPP_GAME_EVENT_H
