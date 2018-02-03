b7a2f659afea8fea6ff9b1354eb7e487a92d98a9
Kiarash Korki <kiarash@korki.me>
2018-02-03 20:48:23 +0330

    Change default port to 7099

--- client/src/Main.cpp
+++ client/src/Main.cpp
@@ -7,7 +7,7 @@
 int main(int argc, char** argv) {
     try {
         std::string host = argc > 1 ? argv[1] : "localhost";
-        int port = argc > 2 ? std::stoi(argv[2]) : 8080;
+        int port = argc > 2 ? std::stoi(argv[2]) : 7099;
 
         auto controller = std::make_unique<Controller>(host, port);
         controller->run();
