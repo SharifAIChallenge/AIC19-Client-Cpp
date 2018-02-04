102b3bc8856f277e52c3feaa302cfe4aa3371e74
Kiarash Korki <kiarash@korki.me>
2018-02-04 12:01:37 +0330

    Implement format_string

--- client/src/Utility.h
+++ client/src/Utility.h
@@ -1,6 +1,10 @@
 #ifndef AIC18_CLIENT_CPP_UTILITY_H
 #define AIC18_CLIENT_CPP_UTILITY_H
 
+#include <cstdio>
+#include <cstring>
+#include <cstdarg>
+
 #include <iostream>
 #include <vector>
 
@@ -23,6 +27,20 @@ std::vector<T> flatten_list(const std::vector<std::vector<T>>& input) {
     for (const auto& row : input)
         result.insert(result.end(), row.begin(), row.end());
     return result;
+}
+
+inline std::string format_string(const std::string& format, ...) {
+    constexpr size_t BUFFER_LEN = 512;
+    static char buffer[BUFFER_LEN];
+
+    memset(buffer, 0, BUFFER_LEN);
+
+    va_list vl;
+    va_start(vl, format);
+    vsnprintf(buffer, BUFFER_LEN, format.c_str(), vl);
+    va_end(vl);
+
+    return std::string(buffer);
 }
 
 extern bool global_verbose_flag;
