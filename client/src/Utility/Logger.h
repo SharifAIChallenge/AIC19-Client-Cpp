6a3c6eb6aae9af01ced00d659a286607be7706b8
Kiarash Korki <kiarash@korki.me>
2018-02-06 23:49:21 +0330

    Improve logger interface

--- client/src/Utility/Logger.h
+++ client/src/Utility/Logger.h
@@ -19,13 +19,15 @@ public:
     Logger(const Logger&) = delete;
     Logger& operator=(const Logger&) = delete;
 
-    inline static Logger& Get(LogLevel level) {
-        static Logger instance;
+    inline static Logger& Get() {
+        return instance;
+    }
 
+    inline static Logger& Get(LogLevel level) {
         instance.m_current_level = level;
 
         constexpr const char* LOG_LEVEL_STRING[] = {"Debug", "Info", "Warning", "Error"};
-        constexpr static size_t INDENT_WIDTH = 12;
+        constexpr size_t INDENT_WIDTH = 12;
 
         std::string line = "[" + std::string(LOG_LEVEL_STRING[level]) + "]";
         line.insert(line.end(), INDENT_WIDTH - line.size(), ' ');
@@ -37,7 +39,7 @@ public:
     template <class Type>
     inline Logger& operator<< (const Type& message) {
 #ifdef AIC_CLIENT_DEBUG
-        if (m_current_level >= STDERR_MIN_LEVEL)
+        if (m_current_level >= m_stderr_min_level)
             std::cerr << message;
         m_output_file << message;
 #endif
@@ -46,29 +48,37 @@ public:
 
     inline Logger& operator<< (std::ostream&(*f)(std::ostream&)) {
 #ifdef AIC_CLIENT_DEBUG
-        if (m_current_level >= STDERR_MIN_LEVEL)
+        if (m_current_level >= m_stderr_min_level)
             f(std::cerr);
         f(m_output_file);
 #endif
         return *this;
     }
 
-    /// Minimum severity level for logs shown in standard error stream
-    static LogLevel STDERR_MIN_LEVEL = LogLevel::WARNING;
+    inline void set_stderr_min_level(LogLevel level) {
+        m_stderr_min_level = level;
+    }
 
 private:
 
     inline Logger()
             : m_output_file("client.log"),
-              m_current_level(DEBUG)
+              m_current_level(DEBUG),
+              m_stderr_min_level(WARNING)
     {
     }
+
+    /// Singleton instance
+    static Logger instance;
 
     /// The output file used for storing log
     std::ofstream m_output_file;
 
     /// Last call to @see Get will set this parameter
     LogLevel m_current_level;
+
+    /// Minimum severity level for logs shown in standard error stream
+    LogLevel m_stderr_min_level;
 
 };
 
