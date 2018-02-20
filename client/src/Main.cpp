4ea9fe972306e9e5e0c3e0fe26d9db36cc651f7d
Kiarash Korki <kiarash@korki.me>
2018-02-20 20:58:03 +0330

    Bump version to 3.0.0

--- client/src/Main.cpp
+++ client/src/Main.cpp
@@ -19,7 +19,7 @@
 #include <Network/NetworkError.h>
 #include <Core/Message/ParseError.h>
 
-constexpr const char* AIC_CLIENT_VERSION = "v2.2.0";
+constexpr const char* AIC_CLIENT_VERSION = "v3.0.0";
 
 bool global_verbose_flag = false;
 
