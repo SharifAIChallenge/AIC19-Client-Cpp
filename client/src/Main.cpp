bef36d33f6d5b5b00ee2328ab470a97b47912af0
dot_blue <onajafi141@gmail.com>
2019-03-13 08:14:28 +0330

    Update Version

--- client/src/Main.cpp
+++ client/src/Main.cpp
@@ -19,7 +19,7 @@
 #include <Network/NetworkError.h>
 #include <Core/Message/ParseError.h>
 
-constexpr const char* AIC_CLIENT_VERSION = "v1.2.1";
+constexpr const char* AIC_CLIENT_VERSION = "v2.0.0";
 
 bool global_verbose_flag = false;
 
